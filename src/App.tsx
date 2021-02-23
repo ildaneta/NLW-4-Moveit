9dee6f60e3c15f3f7e957e10434e348df2b81e70
Ilda Neta <iildaemanoely@gmail.com>
2021-02-22 23:44:33 -0300

    chore: Deleting some files unused

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello NLW
-        </p>
-      </header>
-    </div>
+    <h1>Hello world</h1>
   );
 }
 
